aee27071483d7a2f1cf881e3428884c225fc4081
rakibtg <rakibtg@gmail.com>
2020-07-14 10:51:17 +0600

    updating host and port

--- src/reloader.php
+++ src/reloader.php
@@ -5,34 +5,8 @@
   require_once __DIR__ . '/../env.php';
   $devUrl = isset($_GET['url']) ? $_GET['url'] : false;
   $projectPath = isset($_GET['path']) ? $_GET['path'] : false;
-
-  function is_sub_dir ($path = NULL, $parent_folder = SITE_PATH) {
-
-    //Get directory path minus last folder
-    $dir = dirname($path);
-    $folder = substr($path, strlen($dir));
-
-    //Check the the base dir is valid
-    $dir = realpath($dir);
-
-    //Only allow valid filename characters
-    $folder = preg_replace('/[^a-z0-9\.\-_]/i', '', $folder);
-
-    //If this is a bad path or a bad end folder name
-    if( !$dir OR !$folder OR $folder === '.') {
-      return FALSE;
-    }
-
-    //Rebuild path
-    $path = $dir. DS. $folder;
-
-    //If this path is higher than the parent folder
-    if( strcasecmp($path, $parent_folder) > 0 ) {
-      return $path;
-    }
-
-    return FALSE;
-  }
+  $_port = $env['port'] ? $env['port'] : 2020;
+  $_host = $env['host'] ? $env['host'] : '127.0.0.1';
 
   function get_modification_time ($path) {
     $directory = new RecursiveDirectoryIterator(
@@ -46,13 +20,7 @@
       RecursiveIteratorIterator::SELF_FIRST 
     );
     $resultFile = $iterator->current();
-    $toIgnore = $path . "/vendor/";
     foreach($iterator as $file) {
-      if (is_sub_dir($file, $toIgnore)) {
-        // echo '1, ';
-      } else {
-        echo $file . "\n";
-      }
       if ($file->getMtime() > $resultFile->getMtime()) {
         $resultFile = $file;
       }
@@ -63,15 +31,12 @@
   function prepare_environment () {
     if (!file_exists(__DIR__ . '/../env.php')) {
       copy(__DIR__.'/env.backup', __DIR__.'/../env.php');
-      echo "Created ENV file.\n";
     }
   }
 
   function serve () {
-    global $env;
+    global $env, $_port, $_host;
     $_php = $env['php'] ? $env['php'] : 'php';
-    $_port = $env['port'] ? $env['port'] : 2020;
-    $_host = $env['host'] ? $env['host'] : '127.0.0.1';
     echo "Starting server at: http://$_host:$_port \n";
     $appDir = __DIR__ . '/app';
     exec("$_php -S $_host:$_port -t $appDir");
