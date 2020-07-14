60ca0f37f21277a5292d4578cc3bed5963c3db30
rakibtg <rakibtg@gmail.com>
2020-07-14 17:43:17 +0600

    added ignored file support

--- src/reloader.php
+++ src/reloader.php
@@ -3,10 +3,25 @@
   $envExists = file_exists(__DIR__ . '/../env.php');
   if (!$envExists) prepare_environment();
   require_once __DIR__ . '/../env.php';
-  $devUrl = isset($_GET['url']) ? $_GET['url'] : false;
-  $projectPath = isset($_GET['path']) ? $_GET['path'] : false;
-  $_port = $env['port'] ? $env['port'] : 2020;
-  $_host = $env['host'] ? $env['host'] : '127.0.0.1';
+
+  $devUrl       = isset($_GET['url']) ? $_GET['url'] : false;
+  $projectPath  = isset($_GET['path']) ? rtrim($_GET['path'], DIRECTORY_SEPARATOR) : false;
+  $_port        = $env['port'] ? $env['port'] : 2020;
+  $_host        = $env['host'] ? $env['host'] : '127.0.0.1';
+
+  function is_ignored_file ($filePath) {
+    global $env, $projectPath;
+    foreach ($env['ignore'] as $ignorePath) {
+      $ignorePath = trim($ignorePath, DIRECTORY_SEPARATOR);
+      $ignoreFullpath = $projectPath
+        . DIRECTORY_SEPARATOR
+        . $ignorePath;
+      if (strstr($filePath, $ignoreFullpath)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
   function get_modification_time ($path) {
     $directory = new RecursiveDirectoryIterator(
@@ -21,8 +36,10 @@
     );
     $resultFile = $iterator->current();
     foreach($iterator as $file) {
-      if ($file->getMtime() > $resultFile->getMtime()) {
-        $resultFile = $file;
+      if (!is_ignored_file($file)) {
+        if ($file->getMtime() > $resultFile->getMtime()) {
+          $resultFile = $file;
+        }
       }
     }
     return $resultFile->getMtime();
